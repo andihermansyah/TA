0f8f99034147177e3e544b4a3696f30341acefbf
Andi <andi.hermansyah@gmail.com>
2014-11-04 23:27:33 +0700

    First Commit

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+define('CONSUMER_KEY', 'amKiRhdx2MjQBYIhYJaKQ');
+define('CONSUMER_SECRET', '6sqtZwugXJgeAyWILHmfqrkVAdbdC07mDUc9N9KybI');
+define('OAUTH_ACCESS_TOKEN', '1365397849-AbGVsUU5uAKy1k8C7sEhuyj8cCLi3itZCoyLdye');
+define('OAUTH_ACCESS_TOKEN_SECRET', '9YfJpHMAphKFUIMHpApVJmV6HbbSUsKSu0MgmLkRCaAy0');
+define('OAUTH_CALLBACK', '');
+
+?>
